aef1ddc69102291c9606f85e660a09e243e4aeac
azalinux <52433182+azalinux@users.noreply.github.com>
2019-10-20 09:37:49 +1000

    Update gnome_setup.sh

--- gnome_setup.sh
+++ gnome_setup.sh
@@ -82,7 +82,7 @@ sudo pacman -S --noconfirm --needed sane
 sudo pacman -S --noconfirm --needed sysstat
 sudo pacman -S --noconfirm --needed screenfetch
 sudo pacman -S --noconfirm --needed nautilus-image-converter
-
+sudo pacman -S --noconfirm --needed ipset
 sudo pacman -S --noconfirm --needed galculator
 sudo pacman -S --noconfirm --needed curl
 sudo pacman -S --noconfirm --needed dmidecode
